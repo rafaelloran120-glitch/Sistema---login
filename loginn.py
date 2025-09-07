1b9237a40433e51852c6ab2856e469ff5a28e457
Rafael <rafael@localhost>
2025-09-07 06:13:11 +0000

    Adiciona loginn.py e README.md

---
+++ loginn.py
@@ -0,0 +1,10 @@
+USUARIO =input("USUARIO:")
+print()
+print("BEM VINDO:",USUARIO)
+print()
+SENHA = int(input("DIGITE A SENHA:"))
+print()
+if USUARIO == "RAFAEL" and SENHA == 1010:
+    print("LOGIN EFETUADO")
+else:
+    print("SENHA OU USUARIO INCORRETO,TENTE NOVAMENTE")
